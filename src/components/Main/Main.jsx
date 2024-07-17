a23395e12ccbdb05fead32c743b7d56153afd213
Gokul <gokulchandan24@gmail.com>
2024-07-17 14:04:23 +0530

    created main bar

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,9 +6,35 @@ const Main = () => {
   return (
     <>
       <div className="main">
-        <div className="nav">
-          <p>Gemini</p>
-          <img src={assets.user_icon} alt="" />
+        <div className="main-container">
+          <div className="nav">
+            <p>Gemini</p>
+            <img src={assets.user_icon} alt="" />
+          </div>
+          <div className="greet">
+            <p>
+              <span>Hello</span>
+            </p>
+            <p>How can I help you?</p>
+          </div>
+          <div className="cards">
+            <div className="card">
+              <p>Suggest beautiful places to see on an upcoming road trip</p>
+              <img src={assets.compass_icon} alt="" />
+            </div>
+            <div className="card">
+              <p>Briefly summarize this concept: urban planning</p>
+              <img src={assets.bulb_icon} alt="" />
+            </div>
+            <div className="card">
+              <p>Brainstorming team bonding activities for our work retreat</p>
+              <img src={assets.message_icon} alt="" />
+            </div>
+            <div className="card">
+              <p>Improve the readability of the following code</p>
+              <img src={assets.code_icon} alt="" />
+            </div>
+          </div>
         </div>
       </div>
     </>
